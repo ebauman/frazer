4ef488b3ce9888a237a01cedb04239fe53efb9c3
Eamon Bauman <eamon@eamonbauman.com>
2020-11-12 12:25:37 -0600

    documented example a bit

--- example/api/v1/foo/foo.go
+++ example/api/v1/foo/foo.go
@@ -10,6 +10,7 @@ import (
 type Server struct {
 }
 
+// Example of a handler instantiator.
 func (s *Server) Update() (*apiserver.HandlerOptions, func(context.Context, types.Foo) (types.Foo, error)) {
 	h := &apiserver.HandlerOptions{
 		Path:   "/eamon/update/foo",
@@ -20,6 +21,8 @@ func (s *Server) Update() (*apiserver.HandlerOptions, func(context.Context, type
 	}
 }
 
+// Example of a plain server handler. Added "Calling SERVER" to demonstrate priority of handler calls
+// when two paths are registered, one with RegisterHandler, one with RegisterServer (handler takes priority)
 func (s *Server) Create(ctx context.Context, f types.Foo) (types.Foo, error) {
 	fmt.Println("Calling SERVER")
 	return f, nil
